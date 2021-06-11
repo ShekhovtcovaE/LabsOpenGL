5c80bcb8120fbb88fcf0e8865044c03aac68589f
Elizaveta_Shekhovtcova <54999570+ShekhovtcovaE@users.noreply.github.com>
2021-06-11 14:08:05 +0300

    Create Image.cpp

---
+++ lab4/30/Image.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "image.h"
+
+
+static unsigned int getint(FILE *fp)
+{
+  int c, c1, c2, c3;
+
+ 
+  c = getc(fp);  
+  c1 = getc(fp);  
+  c2 = getc(fp);  
+  c3 = getc(fp);
+  
+  return ((unsigned int) c) +   
+    (((unsigned int) c1) << 8) + 
+    (((unsigned int) c2) << 16) +
+    (((unsigned int) c3) << 24);
+}
+
+static unsigned int getshort(FILE *fp)
+{
+  int c, c1;
+  
+
+  c = getc(fp);  
+  c1 = getc(fp);
+ 
+  return ((unsigned int) c) + (((unsigned int) c1) << 8);
+ 
+}
+    
+
+int ImageLoad(char *filename, Image *image) {
+    FILE *file;
+    unsigned long size;                
+    unsigned long i;                   
+    unsigned short int planes;         
+    unsigned short int bpp;            
+    char temp;                          
+
+    
+    if ((file = fopen(filename, "rb"))==NULL) {
+      printf("File Not Found : %s\n",filename);
+      return 0;
+    }
+    
+    
+    fseek(file, 18, SEEK_CUR);
+    image->sizeX = getint (file);
+    printf("Width of %s: %lu\n", filename, image->sizeX);
+
+    image->sizeY = getint (file);
+    printf("Height of %s: %lu\n", filename, image->sizeY);
+    
+    size = image->sizeX * image->sizeY * 3;
+
+    planes = getshort(file);
+    if (planes != 1) {
+	printf("Planes from %s is not 1: %u\n", filename, planes);
+	return 0;
+    }
+
+
+    bpp = getshort(file);
+    if (bpp != 24) {
+	printf("Bpp from %s is not 24: %u\n", filename, bpp);
+	return 0;
+    }
+    fseek(file, 24, SEEK_CUR);
+
+    image->data = (char *) malloc(size);
+    if (image->data == NULL) {
+	printf("Error allocating memory for color-corrected image data");
+	return 0;	
+    }
+
+    if ((i = fread(image->data, size, 1, file)) != 1) {
+	printf("Error reading image data from %s.\n", filename);
+	return 0;
+    }
+
+    for (i=0;i<size;i+=3) { 
+	temp = image->data[i];
+	image->data[i] = image->data[i+2];
+	image->data[i+2] = temp;
+    }
+
+    return 1;
+}
